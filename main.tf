27cb71e74c86afcde9dd7de3dd27441e976adbcc
Avinash Gunjal <avinashgunjal39@gmail.com>
2025-06-29 19:32:08 +0530

    Added placeholder for security group

--- main.tf
+++ main.tf
@@ -6,3 +6,27 @@ resource "aws_instance" "my_ec2" {
     Name = var.instance_name
   }
 }
+# Placeholder for security group resource
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-xxxxxxx" # dummy or var if needed
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Allow SSH"
+  }
+}
